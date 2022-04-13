c29930465f0c6f7ac5b3b8c1aec6e2744b36bca3
luciano.netto <luciano.netto@i-value.com.br>
2022-04-13 02:22:26 -0300

    loading

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,5 @@
 import styles from './card.module.sass'
+import loader from '../../assets/loader.svg';
 
 export function Card(props) {
     return (
@@ -6,28 +7,35 @@ export function Card(props) {
             <div className={styles.title}>
                 {props.item.name}, {props.item.sys.country}
             </div>
-            <div className={styles.content}>
-                <div className={`${styles.temperature} ${styles[props.item.color]}`}>
-                    {props.item.main.temp.toFixed(0)}<span>o</span>
+            {props.loading && <>
+                <div className={styles.loading}>
+                    <img src={loader} className={styles.loader} alt="Loading" />
                 </div>
-            </div>
-            <div className={styles.info}>
-                <div className={styles.position}>
-                    <div className={styles.values}>
-                        <div className={styles.label}>
-                            HUMIDITY
-                            <div className={styles.value}>{props.item.main.humidity}<span>%</span></div>
-                        </div>
-                        <div className={styles.label}>
-                            PRESSURE
-                            <div className={styles.value}>{props.item.main.pressure}<span>hPa</span></div>
-                        </div>
-                    </div>
-                    <div className={styles.updated}>
-                        Updated at {props.item.updated}
+            </>}
+            {!props.loading && <>
+                <div className={styles.content}>
+                    <div className={`${styles.temperature} ${styles[props.item.color]}`}>
+                        {props.item.main.temp.toFixed(0)}<span>o</span>
                     </div>
                 </div>
-            </div>
+                <div className={styles.info}>
+                    <div className={styles.position}>
+                        <div className={styles.values}>
+                            <div className={styles.label}>
+                                HUMIDITY
+                                <div className={styles.value}>{props.item.main.humidity}<span>%</span></div>
+                            </div>
+                            <div className={styles.label}>
+                                PRESSURE
+                                <div className={styles.value}>{props.item.main.pressure}<span>hPa</span></div>
+                            </div>
+                        </div>
+                        <div className={styles.updated}>
+                            Updated at {props.item.updated}
+                        </div>
+                    </div>
+                </div>
+            </>}
         </div>
     )
 }
--- src/components/Climates/index.jsx
+++ src/components/Climates/index.jsx
@@ -5,7 +5,7 @@ import styles from './climates.module.sass'
 import loader from '../../assets/loader.svg';
 
 export function Climates() {
-    const {data, isLoading, error} = useQuery('climates', async () => {
+    const {data, isLoading, isFetching, error} = useQuery('climates', async () => {
         const { data } = await api.get(`group?id=3421319,3445709,184745&appid=${process.env.REACT_APP_API_KEY}&units=metric`);
         const climates = data.list.map(d => {
             return {
@@ -34,11 +34,14 @@ export function Climates() {
             {isLoading ? (
                 <img src={loader} className={styles.loader} alt="Loading" />
             ) : error ? (
-                <p>Error</p>
+                <div className={styles.error}>
+                    <div className={styles.message}>Something went wrong</div>
+                    <button className={styles.button} type="button" onClick={() => window.location.reload(false)}>Try Again</button>
+                </div>
             ) : (
                 <div className={styles.list}>
                     {data.map((item) => (
-                        <Card key={item.id} item={item} />
+                        <Card key={item.id} item={item} loading={isFetching} />
                     ))}
                 </div>
             )}
